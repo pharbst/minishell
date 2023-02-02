0eec30cb2356b7cc526c192d25a3503fcdc56a5c
Peter Harbst <pharbst@student.42heilbronn.de>
2023-02-02 05:55:22 +0100

    fixed pipex and wrote a Makefile for it the functionality is not tested

---
+++ pipex/includes/pipex.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 19:25:24 by sschanga          #+#    #+#             */
+/*   Updated: 2023/02/02 05:52:12 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libftio.h"
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+
+typedef struct s_cmd
+{
+	char	**cmd;
+	char	*cmd_argv;
+}	t_cmd;
+
+typedef struct s_path
+{
+	int		fd[2];
+	t_cmd	cmds[2];
+	t_cmd	cmd_path[2];
+	char	**path;
+	char	*infile;
+	char	*outfile;
+	int		infile_fd;
+	int		outfile_fd;
+}	t_path;
+
+//Pipex
+// static char	*find_path(char **path);
+void		parse_argv(t_path *pipex, char **argv, char **envp);
+// static char	*set_argv(char **path, char *cmd);
+void		set_pipe(t_path *pipex, char **envp);
+void		argv_child(t_path *pipex, char **envp);
+void		argv_parent(t_path *pipex, char **envp);
+//Protect
+void		set_null(t_path *pipex);
+void		perror_msg(void);
+#endif
---
+++ pipex/src/pipex_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 22:07:33 by sschanga          #+#    #+#             */
+/*   Updated: 2023/02/02 05:50:31 by pharbst          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static char	*set_argv(char **path, char *cmd)
+{
+	char	*add_slash;
+	char	*add_argv;
+	// t_path	pipex;
+
+	while (*path != NULL)
+	{
+		add_slash = ft_strjoin(*path, "/");
+		if (!add_slash)
+			perror_msg();
+		add_argv = ft_strjoin(add_slash, cmd);
+		free(add_slash);
+		if (!add_argv)
+			perror_msg();
+		if (access(add_argv, F_OK) == 0)
+		{
+			cmd = ft_strdup(add_argv);
+			free(add_argv);
+			free(cmd);
+			return (cmd);
+		}
+		free(add_argv);
+		path++;
+	}
+	return (NULL);
+}
+
+void	set_null(t_path *pipex)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < 2)
+	{
+		pipex->cmds[i].cmd = NULL;
+		pipex->cmd_path[i].cmd_argv = NULL;
+	}
+	pipex->path = NULL;
+	pipex->infile = NULL;
+	pipex->outfile = NULL;
+}
+
+void	argv_child(t_path *pipex, char **envp)
+{
+	dup2(pipex->fd[1], STDOUT_FILENO);
+	close(pipex->fd[0]);
+	pipex->infile_fd = open(pipex->infile, O_RDONLY, 0777);
+	if (pipex->infile_fd == -1)
+		perror_msg();
+	dup2(pipex->infile_fd, STDIN_FILENO);
+	close(pipex->fd[1]);
+	close(pipex->infile_fd);
+	pipex->cmd_path[0].cmd_argv = set_argv(pipex->path, *pipex->cmds[0].cmd);
+	execve(pipex->cmd_path[0].cmd_argv, pipex->cmds[0].cmd, envp);
+	perror_msg();
+}
+
+void	argv_parent(t_path *pipex, char **envp)
+{
+	dup2(pipex->fd[0], STDIN_FILENO);
+	close(pipex->fd[1]);
+	pipex->outfile_fd
+		= open(pipex->outfile, O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	dup2(pipex->outfile_fd, STDOUT_FILENO);
+	if (pipex->outfile_fd == -1)
+		perror_msg();
+	close(pipex->fd[0]);
+	close(pipex->outfile_fd);
+	pipex->cmd_path[1].cmd_argv = set_argv(pipex->path, *pipex->cmds[1].cmd);
+	execve(pipex->cmd_path[1].cmd_argv, pipex->cmds[1].cmd, envp);
+	perror_msg();
+}
