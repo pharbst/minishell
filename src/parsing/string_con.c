8cd5294a7b74a39030dd8d81f723db33e2704251
Peter Harbst <pharbst@student.42heilbronn.de>
2023-02-17 13:02:40 +0100

    save commit: rewrote the string condition 	now it is handling dquotes squotes dollar and strings 	it cats everything together until there is a space token and 	saves it to the correct spot in the pipex struct
    	some subfunctions are missing since i haven't wrote them yet

--- src/parsing/string_con.c
+++ src/parsing/string_con.c
@@ -6,93 +6,60 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 14:08:01 by pharbst           #+#    #+#             */
-/*   Updated: 2023/02/17 11:52:51 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/02/17 13:00:58 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	string_cmd_arg_con(t_parsing *a, bool *cmd)
+char	*str_cat(t_parsing *a)
 {
-	if (*a->token_index >= a->token_count
-		|| a->token[*a->token_index].type == SPACE
-		|| a->token[*a->token_index].type == PIPE
-		|| *a->token[*a->token_index].location == '<')
-	{
-		if (*cmd == false)
-			a->pipex->cmd = ft_substr(a->token[*a->token_index - 2].location, 0,
-					(a->token[*a->token_index - 2].location
-						- a->token[*a->token_index - 1].location) + 1);
-		else
-			a->pipex->args = add_arg(a->pipex->args,
-					ft_substr(a->token[*a->token_index - 2].location, 0,
-						(a->token[*a->token_index - 2].location
-							- a->token[*a->token_index -1].location) + 1));
-	}
-}
+	char	*tmp;
 
-static void	string_redirect_con(t_parsing *a, bool *cmd)
-{
-	if (*a->token[*a->token_index].location == '>')
+	tmp = NULL;
+	while (*a->token_index < a->token_count && a->token[*a->token_index].type
+		!= REDIRECT_OUT && a->token[*a->token_index].type != PIPE
+		&& a->token[*a->token_index].type != SPACE)
 	{
-		if (/*is filediscriptor*/)					//if it is a filedescriptor its part of the redirection
-			printf("filediscriptor found\n");
-		else if (*cmd == true)						//when cmd was already found its part of the redirection
+		if (a->token[*a->token_index].type == STRING_OPEN)
 		{
-			//its part of the redirection
+			tmp = strjoinfree(tmp, ft_substr(a->token[*a->token_index].location,
+						0, (a->token[*a->token_index + 1].location
+							- a->token[*a->token_index].location) + 1));
+			*a->token_index += 2;
 		}
-		else
-			a->pipex->cmd = ft_substr(a->token[*a->token_index - 2].location, 0,
-					(a->token[*a->token_index - 2].location
-						- a->token[*a->token_index - 1].location) + 1);
+		else if (a->token[*a->token_index].type == DQUOTE_OPEN)
+			tmp = strjoinfree(tmp, dquote_expand(a));
+		else if (a->token[*a->token_index].type == SQUOTE_OPEN)
+			tmp = strjonfree(tmp, squote_expand(a));
+		else if (a->token[*a->token_index].type == DOLLAR)
+			tmp = strjoinfree(tmp, dollar_expand(a));
+		if (!tmp)
+			return (printf("ERROR: malloc failed in str_cat"), NULL);
 	}
-}
-
-static void	string_dollar_con(t_parsing *a, bool *cmd)
-{
-	if (a->token[*a->token_index].type == DOLLAR)
-		//cat string with variable string together
-}
-
-static void	string_quotes_con(t_parsing *a, bool *cmd)
-{
-	if (a->token[*a->token_index].type == DQUOTE_OPEN)
-		//cat strings together
-	else if (a->token[*a->token_index].type == SQUOTE_OPEN)
-		//cat strings together
-}
-
-static bool	str_cat(t_parsing *a, char *tmp)
-{
-	if (*a->token_index >= a->token_count)
-		return (true);
-	if (a->token[*a->token_index].type == DQUOTE_OPEN)
-		tmp = ft_strjoinfree(tmp, quote_expand(a));
+	return (tmp);
 }
 
 void	string_condition(t_parsing *a, bool *cmd)
 {
 	char	*tmp;
 
-	tmp = str_cat(a, cmd);
+	tmp = str_cat(a);
 	if (!tmp)
 		return (printf("Error from str_cat tmp = NULL\n"));
-	if (a->token[*a->token_index + 1].type == STRING_CLOSE)
-		*a->token_index += 2;
+	else if (*a->token_index < a->token_count && validate_fd(tmp)
+		&& a->token[*a->token_index].type == REDIRECT_OUT)
+		printf("redirect filediscriptor\n");
+	else if (*a->token_index < a->token_count && *cmd == true
+		&& a->token[*a->token_index + 1].type == REDIRECT_OUT)
+		printf("redirect file\n");
+	else if (*cmd == false)
+	{
+		a->pipex->cmd == tmp;
+		*cmd = true;
+	}
 	else
-		return (printf("string not closed\n"));
-	tmp = ft_substr(a->token[*a->token_index - 2].location, 0,
-			(a->token[*a->token_index - 2].location
-				- a->token[*a->token_index - 1].location) + 1);
-	while (!str_cat(a, tmp))
-		continue ;
-	//check what the string is for and save it in the right place
-	if (*a->token_index >= a->token_count)
-		return (string_space_pipe_con(a, cmd));
-	string_space_pipe_con(a, cmd);
-	string_redirect_con(a, cmd);
-	string_dollar_con(a, cmd);
-	string_quotes_con(a, cmd);
+		a->pipex->args = join_arg(a, tmp);
 }
 
-//before > only filediscriptors without space are allowed strings before > and before cmd are ignored
+//before cmd was found only fd without space are allowed before >
