94ebaebdd52009c89eb37c6ee3fedfccf98184cf
Peter Harbst <pharbst@student.42heilbronn.de>
2023-02-22 15:50:23 +0100

    finished:	pwd.c should be done

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,10 +6,19 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 16:10:58 by pharbst           #+#    #+#             */
-/*   Updated: 2023/02/19 16:14:17 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/02/22 14:47:53 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	pwd(t_shell *shell)
+{
+	char	*pwd;
 
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		return (0);
+	printf("%s\n", pwd);
+	return (free(pwd), 0);
+}
