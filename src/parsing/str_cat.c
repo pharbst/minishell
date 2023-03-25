f7cb35e720374a456d37149963f95b77df67776a
Peter Harbst <pharbst@student.42heilbronn.de>
2023-03-26 00:20:13 +0100

    fix:	$ var-name is delimited by char which is not alphabetic or 	numeric

--- src/parsing/str_cat.c
+++ src/parsing/str_cat.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 14:53:49 by pharbst           #+#    #+#             */
-/*   Updated: 2023/03/23 09:47:10 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/03/25 23:30:12 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,38 @@
 
 static char	*dollar_helper(t_parsing *a, char *tmp)
 {
-	if (a->token[a->token_index].type == STRING_OPEN)
-	{
-		tmp = strjoinfree(tmp, ft_substr(a->token[a->token_index].location,
-					0, (a->token[a->token_index + 1].location
-						- a->token[a->token_index].location) + 1));
+	int		i;
+	char	*tmp2;
+
+	i = 0;
+	if (a->token[a->token_index].location[0] == '?')
+		return (strjoinfree(tmp, ft_itoa(a->exit_status)));
+	while (ft_isalnum(a->token[a->token_index].location[i]))
+		i++;
+	tmp2 = ft_substr(a->token[a->token_index].location, 0, i);
+	tmp = strjoinfree(tmp, get_var_content(a->envp, tmp2));
+	free(tmp2);
+	if (&a->token[a->token_index].location[i]
+		< a->token[a->token_index + 1].location)
+		a->token[a->token_index].location
+			= &a->token[a->token_index].location[i];
+	else
 		a->token_index += 2;
-	}
-	else if (a->token[a->token_index].type == DQUOTE_OPEN)
-		tmp = strjoinfree(tmp, quote_expand(a));
-	else if (a->token[a->token_index].type == SQUOTE_OPEN)
-		tmp = strjoinfree(tmp, quote_expand(a));
-	if (tmp && *tmp == '?')
-		return (free(tmp), ft_itoa(a->exit_status));
-	return (get_var_content(a->envp, tmp));
+	// if (a->token[a->token_index].type == STRING_OPEN)
+	// {
+	// 	tmp = strjoinfree(tmp, ft_substr(a->token[a->token_index].location,
+	// 				0, (a->token[a->token_index + 1].location
+	// 					- a->token[a->token_index].location) + 1));
+	// 	a->token_index += 2;
+	// }
+	// else if (a->token[a->token_index].type == DQUOTE_OPEN)
+	// 	tmp = strjoinfree(tmp, quote_expand(a));
+	// else if (a->token[a->token_index].type == SQUOTE_OPEN)
+	// 	tmp = strjoinfree(tmp, quote_expand(a));
+	// if (tmp && *tmp == '?')
+	// 	return (free(tmp), ft_itoa(a->exit_status));
+	// return (get_var_content(a->envp, tmp));
+	return (tmp);
 }
 
 static char	*str_cat_helper(t_parsing *a, char *tmp)
@@ -49,22 +67,26 @@ static char	*str_cat_helper(t_parsing *a, char *tmp)
 	else if (a->token[a->token_index].type == DOLLAR)
 	{
 		a->token_index++;
-		if (a->token_index < a->token_count
-			&& (a->token[a->token_index].type == STRING_OPEN
-				|| a->token[a->token_index].type == DQUOTE_OPEN
-				|| a->token[a->token_index].type == SQUOTE_OPEN))
-		{
-			while (a->token_index < a->token_count
-				&& a->token[a->token_index].type != REDIRECT_IN
-				&& a->token[a->token_index].type != REDIRECT_OUT
-				&& a->token[a->token_index].type != PIPE
-				&& a->token[a->token_index].type != SPACE_START
-				&& a->token[a->token_index].type != DOLLAR)
-				tmp2 = dollar_helper(a, tmp2);
-			tmp = strjoinfree(tmp, tmp2);
-		}
-		else
-			tmp = ft_strjoinfree(tmp, "$");
+		if (a->token[a->token_index].type != STRING_OPEN)
+			return (ft_strjoinfree(tmp, "$"));
+		tmp = dollar_helper(a, tmp);
+		// a->token_index++;
+		// if (a->token_index < a->token_count
+		// 	&& (a->token[a->token_index].type == STRING_OPEN
+		// 		|| a->token[a->token_index].type == DQUOTE_OPEN
+		// 		|| a->token[a->token_index].type == SQUOTE_OPEN))
+		// {
+		// 	while (a->token_index < a->token_count
+		// 		&& a->token[a->token_index].type != REDIRECT_IN
+		// 		&& a->token[a->token_index].type != REDIRECT_OUT
+		// 		&& a->token[a->token_index].type != PIPE
+		// 		&& a->token[a->token_index].type != SPACE_START
+		// 		&& a->token[a->token_index].type != DOLLAR)
+		// 		tmp2 = dollar_helper(a, tmp2);
+		// 	tmp = strjoinfree(tmp, tmp2);
+		// }
+		// else
+		// 	tmp = ft_strjoinfree(tmp, "$");
 	}
 	return (tmp);
 }
