755f5b43437d6a8c2505773867c822561bfd7810
Catina Compote <ccompote@1-D-2.42heilbronn.de>
2023-03-26 12:37:49 +0200

    unset working

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: ccompote <ccompote@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:24:29 by pharbst           #+#    #+#             */
-/*   Updated: 2023/03/26 12:29:43 by ccompote         ###   ########.fr       */
+/*   Updated: 2023/03/26 12:36:49 by ccompote         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int	unset(t_shell *shell, char **argv)
 	if (argc == 1)
 		return (0);
 	if (!valid_var(argv[1]))
+	{
 		ft_putstrsfd(2, "unset: `", argv[1], VAR_NO_VALID, NULL);
 		return (1);
+	}
 	i = find_var(shell->envp, argv[1]);
 	if (i == -1)
 		return (0);
